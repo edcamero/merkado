c19baa09541cc7c6694b5069b3b73f4066d2bfb4
Enyerson Camero <blade_liger@hotmail.com>
2020-07-31 09:57:08 -0500

    Update script base_datos.sql

--- src/database/script base_datos.sql
+++ src/database/script base_datos.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS usuarios;
 DROP TABLE IF EXISTS productos;
 CREATE TABLE USUARIOS(
-   ID SERIAL,
+   user_id SERIAL,
    username VARCHAR(40) NOT NULL, 
-   password VARCHAR(40) NOT NULL 
+   password VARCHAR(40) NOT NULL,
+   CONSTRAINT usuarios_pk PRIMARY KEY(user_id)
 );
 
 INSERT INTO public.usuarios(username, password)VALUES ('admin', 'admin');
@@ -14,5 +15,6 @@ CREATE TABLE PRODUCTOS(
    prod_precio_compra INT NOT NULL,
    prod_precio_venta INT NOT NULL,
    prod_cantidad INT NOT NULL,
-   prod_descripcion VARCHAR(200)
+   prod_descripcion VARCHAR(200),
+   CONSTRAINT productos_pk PRIMARY KEY(prod_id)
 );
