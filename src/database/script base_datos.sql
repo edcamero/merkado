1827b0ac66f6108c222e1b69cd4fd8419b77fde3
Enyerson Camero <blade_liger@hotmail.com>
2020-07-31 19:44:34 -0500

    trigger de insert de productos

--- src/database/script base_datos.sql
+++ src/database/script base_datos.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS usuarios;
 DROP TABLE IF EXISTS productos;
+
+DROP TABLE IF EXISTS _productos;
+
 CREATE TABLE USUARIOS(
    user_id SERIAL,
    username VARCHAR(40) NOT NULL, 
@@ -17,4 +20,15 @@ CREATE TABLE PRODUCTOS(
    prod_cantidad INT NOT NULL,
    prod_descripcion VARCHAR(200),
    CONSTRAINT productos_pk PRIMARY KEY(prod_id)
+);
+--TABLA PARA AUDITORIA DE PRODUCTOS
+CREATE TABLE _PRODUCTOS(
+   prod_id numeric,
+   prod_nombre VARCHAR(40) NOT NULL, 
+   prod_precio_compra INT NOT NULL,
+   prod_precio_venta INT NOT NULL,
+   prod_cantidad INT NOT NULL,
+   prod_descripcion VARCHAR(200),
+   prod_fecha_operacion DATE not null,
+   prod_operacion VARCHAR(1) NOT NULL
 );
