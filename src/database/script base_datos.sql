96c3df060f657dbc387545362676a440888cb559
Enyerson Camero <blade_liger@hotmail.com>
2020-08-04 01:10:53 -0500

    actualizar y borrar tabla de auditorias de productos

--- src/database/script base_datos.sql
+++ src/database/script base_datos.sql
@@ -1,9 +1,15 @@
 DROP TABLE IF EXISTS usuarios;
 DROP TABLE IF EXISTS productos;
 DROP TABLE IF EXISTS tipo_productos;
+DROP TABLE IF EXISTS _tipo_productos;
 DROP TABLE IF EXISTS _productos;
 
---DROP TABLE IF EXISTS _productos;
+CREATE TABLE TIPO_USUARIOS(
+tius_id SERIAL,
+tius_nombre VARCHAR(40) NOT NULL,
+tius_descripcion VARCHAR(100) NOT NULL,
+CONSTRAINT tipo_usuario_pk PRIMARY KEY(tius_id)
+);
 
 CREATE TABLE USUARIOS(
    user_id SERIAL,
@@ -34,16 +40,33 @@ CREATE TABLE PRODUCTOS(
 
 ------------------------------
 --Auditorias
--------------------------------
+-------------------------------}
+
+--TABLA PARA AUDITORIA DE TIPO_PRODUCTOS
+CREATE TABLE _TIPO_USUARIOS(
+	tius_id SERIAL,
+	tius_nombre VARCHAR(40) NOT NULL,
+	tius_descripcion VARCHAR(100) NOT NULL,
+	tius_fecha_operacion timestamp not null,
+	tius_operacion VARCHAR(1) NOT NULL
+);
+
+CREATE TABLE _USUARIOS(
+	user_id SERIAL,
+	username VARCHAR(40) NOT NULL, 
+	password VARCHAR(40) NOT NULL,   
+	user_fecha_operacion timestamp not null,
+	user_operacion VARCHAR(1) NOT NULL
+);
 
 --TABLA PARA AUDITORIA DE TIPO_PRODUCTOS
 
 CREATE TABLE _TIPO_PRODUCTOS(
-tipr_id SERIAL,
-tipr_nombre VARCHAR(40),
-tipr_descripcion VARCHAR(200),
-tipr_fecha_operacion timestamp not null,
-tipr_operacion VARCHAR(1) NOT NULL
+	tipr_id SERIAL,
+	tipr_nombre VARCHAR(40),
+	tipr_descripcion VARCHAR(200),
+	tipr_fecha_operacion timestamp not null,
+	tipr_operacion VARCHAR(1) NOT NULL
 );
 
 --TABLA PARA AUDITORIA DE PRODUCTOS
--- src/database/triggers de auditoria.sql
+++ src/database/triggers de auditoria.sql
@@ -16,3 +16,41 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER tr_i_productos
  AFTER INSERT ON productos
  FOR EACH ROW EXECUTE PROCEDURE ftr_i_productos();
+ 
+ 
+ --funcion auditoria al update productos
+ CREATE OR REPLACE  FUNCTION ftr_u_productos() RETURNS TRIGGER AS $$
+BEGIN
+INSERT INTO public._productos(
+	prod_id, prod_nombre, prod_precio_compra, prod_precio_venta, prod_cantidad, prod_descripcion, prod_fecha_operacion,prod_operacion)
+	VALUES (new.prod_id, new.prod_nombre, new.prod_precio_compra, new.prod_precio_venta, new.prod_cantidad,new.prod_descripcion, (select current_timestamp),'U');
+
+
+RETURN new ;
+END;
+$$ LANGUAGE plpgsql;
+
+--trigger de update productos
+
+CREATE TRIGGER tr_u_productos
+ AFTER UPDATE ON productos
+ FOR EACH ROW EXECUTE PROCEDURE ftr_u_productos();
+ 
+ 
+ --funcion auditoria al borrar productos 
+ CREATE OR REPLACE  FUNCTION ftr_d_productos() RETURNS TRIGGER AS $$
+BEGIN
+INSERT INTO public._productos(
+	prod_id, prod_nombre, prod_precio_compra, prod_precio_venta, prod_cantidad, prod_descripcion, prod_fecha_operacion,prod_operacion)
+	VALUES (old.prod_id, old.prod_nombre, old.prod_precio_compra, old.prod_precio_venta, old.prod_cantidad,old.prod_descripcion, (select current_timestamp),'D');
+
+
+RETURN new ;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER tr_d_productos
+ AFTER DELETE ON productos
+ FOR EACH ROW EXECUTE PROCEDURE ftr_d_productos();
+ 
